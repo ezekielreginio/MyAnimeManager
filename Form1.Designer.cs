682e2849e9ccdcc55ed711bb049eef62cfdb6325
ezekielreginio <78013755+ezekielreginio@users.noreply.github.com>
2021-12-23 20:44:35 +0800

    first push

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace MyAnimeManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.animeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.currentlyWatchingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.planToWatchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.completedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginToMALToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listViewFiles = new System.Windows.Forms.ListView();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.animeToolStripMenuItem,
+            this.optionsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(826, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.fileToolStripMenuItem.Text = "Home";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // animeToolStripMenuItem
+            // 
+            this.animeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.currentlyWatchingToolStripMenuItem,
+            this.planToWatchToolStripMenuItem,
+            this.completedToolStripMenuItem});
+            this.animeToolStripMenuItem.Name = "animeToolStripMenuItem";
+            this.animeToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.animeToolStripMenuItem.Text = "Anime";
+            // 
+            // currentlyWatchingToolStripMenuItem
+            // 
+            this.currentlyWatchingToolStripMenuItem.Name = "currentlyWatchingToolStripMenuItem";
+            this.currentlyWatchingToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.currentlyWatchingToolStripMenuItem.Text = "MyAnimeList";
+            // 
+            // planToWatchToolStripMenuItem
+            // 
+            this.planToWatchToolStripMenuItem.Name = "planToWatchToolStripMenuItem";
+            this.planToWatchToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.planToWatchToolStripMenuItem.Text = "Top Anime List";
+            // 
+            // completedToolStripMenuItem
+            // 
+            this.completedToolStripMenuItem.Name = "completedToolStripMenuItem";
+            this.completedToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.completedToolStripMenuItem.Text = "Seasonal Anime";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loginToMALToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // loginToMALToolStripMenuItem
+            // 
+            this.loginToMALToolStripMenuItem.Name = "loginToMALToolStripMenuItem";
+            this.loginToMALToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
+            this.loginToMALToolStripMenuItem.Text = "Login to MAL";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(179, 22);
+            this.toolStripMenuItem1.Text = "Set Anime Directory";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(150, 150);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listViewFiles
+            // 
+            this.listViewFiles.LargeImageList = this.imageList1;
+            this.listViewFiles.Location = new System.Drawing.Point(12, 27);
+            this.listViewFiles.Name = "listViewFiles";
+            this.listViewFiles.Size = new System.Drawing.Size(802, 411);
+            this.listViewFiles.TabIndex = 1;
+            this.listViewFiles.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 450);
+            this.Controls.Add(this.listViewFiles);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MyAnimeManager v1.0";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You have clicked Home Button");
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem animeToolStripMenuItem;
+        private ToolStripMenuItem optionsToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem currentlyWatchingToolStripMenuItem;
+        private ToolStripMenuItem planToWatchToolStripMenuItem;
+        private ToolStripMenuItem completedToolStripMenuItem;
+        private ToolStripMenuItem loginToMALToolStripMenuItem;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ImageList imageList1;
+        private ListView listViewFiles;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+using System.Diagnostics;
+using System.Net.Mail;
+using System.Runtime.InteropServices;
+
+namespace MyAnimeManager
+{
+    public partial class Form1 : Form
+    {
+        List<string> listFiles = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SHFILEINFO
+        {
+            public IntPtr hIcon;
+            public int iIcon;
+            public uint dwAttributes;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szDisplayName;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
+            public string szTypeName;
+        };
+
+        class Win32
+        {
+            [DllImport("shell32.dll")]
+            public static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbSizeFileInfo, uint uFlags);
+        }
+
+        //Constants flags for SHGetFileInfo 
+        public const uint SHGFI_ICON = 0x100;
+        public const uint SHGFI_LARGEICON = 0x0; // 'Large icon
+
+        string getIconPath(string folderPath)
+        {
+            SHFILEINFO shinfo = new SHFILEINFO();
+            Win32.SHGetFileInfo(folderPath, 0, ref shinfo, (uint)Marshal.SizeOf(shinfo), (int)0x1000);
+            return shinfo.szDisplayName;
+        }
+
+  
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            listFiles.Clear();
+            listViewFiles.Items.Clear();
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog() { Description = "Select your Anime Directory." })
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string item in Directory.GetDirectories(folderBrowserDialog.SelectedPath))
+                    {
+                        //imageList1.Images.Add(System.Drawing.Icon.ExtractAssociatedIcon(item));
+                        //FileInfo fileInfo = new FileInfo(item);
+                        //listFiles.Add(fileInfo.FullName);
+                        //listViewFiles.Items.Add(fileInfo.Name,imageList1.Images.Count - 1);
+                        //MessageBox.Show("Icon Path: " + getIconPath(directoryInfo.FullName));
+
+                        DirectoryInfo directoryInfo = new DirectoryInfo(item);
+                        Bitmap bp = new Bitmap(getIconPath(directoryInfo.FullName));
+                        imageList1.Images.Add(bp);
+                        MessageBox.Show("Icon Path: " + bp.GetType());
+                        listViewFiles.Items.Add(directoryInfo.Name, imageList1.Images.Count - 1);
+                    }
+                }
+            }
+            //using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog() { Description = "Select your Anime Directory." })
+            //{
+            //    if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            //    {
+            //        foreach (string item in Directory.GetFiles(folderBrowserDialog.SelectedPath))
+            //        {
+            //            imageList1.Images.Add(System.Drawing.Icon.ExtractAssociatedIcon(item));
+            //            FileInfo fi = new FileInfo(item);
+            //            listViewFiles.Items.Add(fi.Name, imageList1.Images.Count - 1);
+            //        }
+            //    }
+            //}
+
+        }
+    }
+}
