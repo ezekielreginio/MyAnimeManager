46f95da22af2225d591b7a141a84a38328256632
ezekielreginio <78013755+ezekielreginio@users.noreply.github.com>
2022-01-10 19:40:49 +0800

    Desktop UI Finished

--- MyAnimeManager 1.0/MyAnimeManager 1.0/Form1.cs
+++ MyAnimeManager 1.0/MyAnimeManager 1.0/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Bunifu.Framework.UI;
+using Bunifu.UI.WinForms.BunifuButton;
+using FontAwesome.Sharp;
+using MyAnimeManager_1._0.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +16,91 @@ namespace MyAnimeManager_1._0
 {
     public partial class Form1 : Form
     {
+        //Fields
+        private IconPictureBox currentButton;
+        private Form activeDesktopForm;
+        private Random random;
+        private int tempIndex;
+
+        //Constructor
         public Form1()
         {
             InitializeComponent();
+            random = new Random();
+            //Open Directory Form
+            OpenChildForm(new Forms.Directory(), null);
+        }
+
+        //Private Methods
+        private void ActivateButton(object btnSender)
+        {
+            if(btnSender != null)
+            {
+                if(currentButton != (IconPictureBox)btnSender)
+                {
+                    DisableButton();
+                    Color color = SelectThemeColor();
+                    currentButton = (IconPictureBox)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.White;
+                }
+            }
+        }
+
+        private void DisableButton()
+        {
+            foreach(Control previousBtn in panelMenu.Controls)
+            {
+                if(previousBtn.GetType() == typeof(IconPictureBox))
+                {
+                    previousBtn.BackColor = Color.FromArgb(51, 51, 76);
+                }
+            }
+        }
+
+        private Color SelectThemeColor()
+        {
+            int index = random.Next(ThemeColor.ColorList.Count);
+            while (tempIndex == ThemeColor.ColorList.Count)
+            {
+                random.Next(ThemeColor.ColorList.Count);
+            }
+            tempIndex = index;
+            string color = ThemeColor.ColorList[index];
+            return ColorTranslator.FromHtml(color);
+        }
+
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            if(activeDesktopForm != null)
+            {
+                activeDesktopForm.Close();
+            }
+            activeDesktopForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.panelDesktop.Controls.Add(childForm);
+            this.panelDesktop.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        //Event Bindings
+        private void iconPictureBox1_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void iconPictureBox2_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+
+        }
+
+        private void iconPictureBox3_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
         }
     }
 }
---
+++ MyAnimeManager 1.0/MyAnimeManager 1.0/Forms/Directory.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MyAnimeManager_1._0.Forms
+{
+    partial class Directory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelNoDirectory = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.buttonSelectDirectory = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelNoDirectory.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelNoDirectory
+            // 
+            this.panelNoDirectory.Controls.Add(this.panel3);
+            this.panelNoDirectory.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelNoDirectory.Location = new System.Drawing.Point(0, 0);
+            this.panelNoDirectory.Name = "panelNoDirectory";
+            this.panelNoDirectory.Size = new System.Drawing.Size(800, 450);
+            this.panelNoDirectory.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(184)))), ((int)(((byte)(108)))));
+            this.panel3.Controls.Add(this.buttonSelectDirectory);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(800, 44);
+            this.panel3.TabIndex = 1;
+            // 
+            // buttonSelectDirectory
+            // 
+            this.buttonSelectDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSelectDirectory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(66)))), ((int)(((byte)(146)))));
+            this.buttonSelectDirectory.FlatAppearance.BorderSize = 0;
+            this.buttonSelectDirectory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSelectDirectory.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSelectDirectory.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonSelectDirectory.Location = new System.Drawing.Point(668, 6);
+            this.buttonSelectDirectory.Name = "buttonSelectDirectory";
+            this.buttonSelectDirectory.Size = new System.Drawing.Size(120, 33);
+            this.buttonSelectDirectory.TabIndex = 1;
+            this.buttonSelectDirectory.Text = "Set Directory";
+            this.buttonSelectDirectory.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.label2.Size = new System.Drawing.Size(230, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Your Anime Directory is Not Set";
+            // 
+            // Directory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelNoDirectory);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Directory";
+            this.Text = "Directory";
+            this.panelNoDirectory.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelNoDirectory;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button buttonSelectDirectory;
+        private System.Windows.Forms.Label label2;
+    }
+}
